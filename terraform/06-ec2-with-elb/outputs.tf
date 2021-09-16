23062d19f1f207e004a98ffb61a34688c5c3c31d
Roberto Fernandez del Valle <rfernandez@alfatecsistemas.es>
2021-09-16 15:36:53 +0200

    cambios 16/09

--- terraform/06-ec2-with-elb/outputs.tf
+++ terraform/06-ec2-with-elb/outputs.tf
@@ -3,9 +3,13 @@ output "aws_security_group_http_server_details" {
 }
 
 output "http_server_details" {
-  value = aws_instance.http_server
+  value = aws_instance.http_servers
 }
 
 output "http_server_public_dns" {
-  value = aws_instance.http_server.public_dns
+  value = values(aws_instance.http_servers).*.id
+}
+
+output "elb_public_dns" {
+  value = aws_elb.elb
 }
